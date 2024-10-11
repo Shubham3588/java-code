9a959aec09ff7d2275a2a79396b72f66bc6ad38d
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2024-10-11 08:59:52 +0530

    Added Java code for Static variable

---
+++ src/staticVariables/staticVariableExampleMainClass.java
@@ -0,0 +1,11 @@
+package staticVariables;
+
+public class staticVariableExampleMainClass {
+    public static void main(String[] args) {
+        staticVariableExample a = new staticVariableExample("a");
+        staticVariableExample b = new staticVariableExample("b");
+        a.printName();
+        b.printName();
+
+    }
+}
